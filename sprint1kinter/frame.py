a6accb6bfe6e5b091ef47af282e6c707a286b0ac
Samupindo <samipindo22@gmail.com>
2025-10-23 11:38:21 +0200

    Hasta ejercicio 9, menú

--- sprint1kinter/frame.py
+++ sprint1kinter/frame.py
@@ -2,6 +2,37 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Ejercicio Frame")
-root.geometry("400x450")
+root.geometry("400x300")
 
+#  FRAME SUPERIOR
+frame_sup = tk.Frame(root, bg="#e0e0e0", bd=2, relief="sunken")
+frame_sup.pack(padx=20, pady=20, fill="both", expand=True)
 
+titulo = tk.Label(frame_sup, text="Introduce un texto:", bg="#e0e0e0")
+titulo.grid(row=0, column=0, padx=10, pady=5)
+
+entry_texto = tk.Entry(frame_sup, width=25)
+entry_texto.grid(row=0, column=1, padx=10, pady=5)
+
+label_resultado = tk.Label(frame_sup, text="Aquí aparecerá tu texto", bg="#e0e0e0")
+label_resultado.grid(row=1, column=0, columnspan=2, pady=10)
+
+#  FRAME INFERIOR
+frame_inf = tk.Frame(root, bg="#e0e0e0", bd=2, relief="sunken")
+frame_inf.pack(fill="x", padx=20, pady=10)
+
+def mostrar_texto():
+    texto = entry_texto.get()
+    label_resultado.config(text=texto if texto else "No has escrito nada")
+
+def borrar_texto():
+    entry_texto.delete(0, tk.END)
+    label_resultado.config(text="Contenido borrado")
+
+button_mostrar = tk.Button(frame_inf, text="Mostrar", command=mostrar_texto)
+button_borrar = tk.Button(frame_inf, text="Borrar", command=borrar_texto)
+
+button_mostrar.grid(row=0, column=0, padx=20, pady=10)
+button_borrar.grid(row=0, column=1, padx=20, pady=10)
+
+root.mainloop()
---
+++ sprint1kinter/menu.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def nueva_ventana():
+    messagebox.showinfo("Este mensaje es de prueba.\n\n"
+                        "                               ")
+
+
+def salir_app():
+    root.quit()
+
+
+
+root= tk.Tk()
+root.title("Ejemplo de Menú")
+root.geometry("300x200")
+
+menu_principal = tk.Menu(root)
+root.config(menu=menu_principal)
+
+menu_archivo = tk.Menu(menu_principal,tearoff=0)
+menu_principal.add_cascade(label="Archivo",menu=menu_archivo)
+menu_archivo.add_command(label="Abrir",command=nueva_ventana)
+menu_archivo.add_separator()
+menu_archivo.add_command(label="Salir",command=salir_app)
+
+def nueva_ventana():
+    messagebox.showinfo("Este mensaje debería ayudarte.\n\n"
+        "                               ")
+
+menu_ayuda = tk.Menu(menu_principal,tearoff=0)
+menu_principal.add_cascade(label="Ayuda",menu=menu_ayuda)
+menu_ayuda.add_command(label="Acerca de",command=nueva_ventana)
+
+
+root.mainloop()
